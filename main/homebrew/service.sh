9a973adacaa44b1d09f91605d1647a83c0b86bce
BWare <dennis.roos@gmail.com>
2017-09-02 10:48:15 +0200

    bugfix

--- main/homebrew/service.sh
+++ main/homebrew/service.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+BASEDIR="$(dirname "$0")"
+
 let MAXCHAR=1476		# 1489 - 13
 INTERVAL=30
 
 OLDIFS=$IFS
 while [ true ]; do
 	# Load configuration
-	if [ -e ./service.cfg ]; then
-		if [ ! -x ./service.cfg ]; then
-			chmod 755 ./service.cfg
+	if [ -e $BASEDIR/service.cfg ]; then
+		if [ ! -x $BASEDIR/service.cfg ]; then
+			chmod 755 $BASEDIR/service.cfg
 		fi
-		. ./service.sh
+		. $BASEDIR/service.cfg
+		echo "(Re-)loaded configuration file."
 	fi
 	IFS=$OLDIFS
 	json_files=(*.json)
@@ -32,7 +35,9 @@ while [ true ]; do
 			printf "Images"
 			printf "hb/image:%s\n" $img_data | netcat -u -q 1 localhost 4444
 			printf " --- DONE!\n\n"
+			printf "(zzZZ) Sleeping for %ss (ZZzz)" "$INTERVAL"
 			sleep $INTERVAL
+			printf "\n\n"
 		else
 			printf " --- Skipping!\n --- Reason: JSON data to big for udp packet (${#data} chars > $MAXCHAR)\n\n"
 		fi
